07c15b9003c6fcddb06c9625ab0846db79ed0264
Arnaud <arnaud.bernard97@gmail.com>
2018-05-24 09:58:08 +0200

    fix(test): removed unused import in persistence test

--- src/test/Persistence_Test1.java
+++ src/test/Persistence_Test1.java
@@ -6,36 +6,18 @@
 package test;
 
 import algo.Algorithme;
-import dao.ArcDao;
-import dao.CommandeDao;
 import dao.DaoFactory;
-import dao.EntrepotDao;
 import dao.InstanceDao;
-import dao.JpaArcDao;
-import dao.JpaCommandeDao;
-import dao.JpaEntrepotDao;
-import dao.JpaInstanceDao;
-import dao.JpaLocalisationDao;
-import dao.JpaProduitDao;
-import dao.LocalisationDao;
 import dao.PersistenceType;
-import dao.ProduitDao;
-import dao.SolutionDao;
 import ecriture.WriteSolution;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import lecture.Result;
 import lecture.ReadFiles;
-import metier.Arc;
 import metier.Commande;
 import metier.Instance;
-import metier.Localisation;
-import metier.Produit;
-import metier.Solution;
 
 /**
  *
