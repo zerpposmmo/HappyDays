5710e163e57a7e17aaf372df7f80b68d5a032dec
SamuelP <samonil@hotmail.fr>
2018-06-14 22:02:25 +0200

    feat(): Création de test6 (test de durée d'execution)et persistence 3 (test v1 en persistence)

--- src/test/Persistence_Test2.java
+++ src/test/Persistence_Test2.java
@@ -27,17 +27,33 @@ public class Persistence_Test2 {
     public static void main(String[] args) {
         Result result;
         ReadFiles readFiles = new ReadFiles();
+        long begin;
+        long end;
         try {
-            result = readFiles.getCreatedObjects("src/files/instance_0116_131950_Z1.txt");
+            String[] fichier = new String[10];
+            fichier[0] = "instance_0116_131940_Z2.txt";
+            fichier[1] = "instance_0116_131950_Z1.txt";
+            fichier[2] = "instance_0130_132439_Z2.txt";
+            fichier[3] = "instance_0202_132568_Z2.txt";
+            fichier[4] = "instance_0203_132623_Z1.txt";
+            fichier[5] = "instance_0214_132873_Z2.txt";
+            fichier[6] = "instance_0215_132916_Z2.txt";
+            fichier[7] = "instance_0606_136170_Z1.txt";
+            fichier[8] = "instance_0606_136175_Z1.txt";
+            fichier[9] = "instance_0606_136178_Z1.txt";
+            
+            result = readFiles.getCreatedObjects("src/files/"+fichier[9]);
             HashSet commandes = new HashSet(result.getCommandes().values());
             Algorithme a = new Algorithme(result.getInstance(), commandes, result);
             Solution bestSolution = null;
             int index = 0;
-            for (; index < 2000; index++) {
-                System.out.println(index);
+            begin = System.currentTimeMillis();
+            for (; index < 200; index++) {
+                // System.out.println(index);
                 a.creerSolutionUpdated();
             }
-
+            end = System.currentTimeMillis();
+            System.out.println("Durée:" + (end - begin));
             for (Solution s : result.getInstance().getSolutionSet()) {
                 if (bestSolution == null) {
                     bestSolution = s;
